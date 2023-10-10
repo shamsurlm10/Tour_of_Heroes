0a3be1bdfa4c02c4d287d1aa1a91ee21d711920e
shamsurlm10 <shamsur.works@gmail.com>
2023-10-10 17:09:02 +0600

    created heroes and showed the list and edit

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,43 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [{
+    id: 1,
+    name: 'Windstorm'
+  },
+  {
+    id: 2,
+    name: 'Bombasto'
+  },
+  {
+    id: 3,
+    name: 'Magneta'
+  },
+  {
+    id: 4,
+    name: 'Tornado'
+  },
+  {
+    id: 5,
+    name: 'Mr. Nice'
+  },
+  {
+    id: 6,
+    name: 'Narco'
+  },
+  {
+    id: 7,
+    name: 'RubberMan'
+  },
+  {
+    id: 8,
+    name: 'Dynama'
+  },
+  {
+    id: 9,
+    name: 'Dr IQ'
+  },
+  {
+    id: 10,
+    name: 'Magma'
+  }
+];
